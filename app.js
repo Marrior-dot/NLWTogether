9f177a26725e38ab925a99d1db1675106bc08d6b
Marrior-dot <mrlimaroberto@gmail.com>
2021-06-25 12:37:29 -0300

    incrementando funcionalidades de alternativas

---
+++ app.js
@@ -0,0 +1,7 @@
+let read = document.getElementsByClassName("read");
+let division = document.getElementsByClassName("question");
+
+read[0].addEventListener("click", function(){
+		division[0].style.backgroundColor = "#E5EAF1";
+})
+
